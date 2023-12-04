cd322cd5fd58b5fb1da73d277be37ef9d18895c8
Ravel <ravelrcamargo@gmail.com>
2023-12-04 14:40:59 -0300

    Checkmate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,10 +57,18 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
+		
+		if(!chessMatch.getCheckMate()) {
 		System.out.println("waiting player: " + chessMatch.getCurrentPlayer());
 		if(chessMatch.getCheck()) {
 			System.out.println("CHECK!");
+			}
 		}
+		else {
+			System.out.println("CHECKMATE!!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		}
+		
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
